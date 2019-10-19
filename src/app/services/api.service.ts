7057b99e00d60ada7b36c6b400e5264c6184e1dc
wnpraf <rapribu@gmail.com>
2019-10-19 20:23:08 +0200

    Making generic api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,21 +1,20 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
-import { Post } from '../models'
 
 @Injectable()
 export default class Api {
   constructor(private http: HttpClient) {}
-  get(url: string): Observable<Post[]> {
-    return this.http.get<Post[]>(url)
+  get<T>(url: string): Observable<T> {
+    return this.http.get<T>(url)
   }
-  post(url: string, post: Post): Observable<Post> {
-    return this.http.post<Post>(url, post)
+  post<T>(url: string, post: T): Observable<T> {
+    return this.http.post<T>(url, post)
   }
-  put(url: string, post: Post): Observable<Post> {
-    return this.http.put<Post>(url, post)
+  put<T>(url: string, post: T): Observable<T> {
+    return this.http.put<T>(url, post)
   }
-  delete(url: string): Observable<Post> {
-    return this.http.delete<Post>(url)
+  delete<T>(url: string): Observable<T> {
+    return this.http.delete<T>(url)
   }
 }
