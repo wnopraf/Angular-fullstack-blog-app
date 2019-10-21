c8d63a51f078c64b979b5c96a34b5444f168e106
wnpraf <rapribu@gmail.com>
2019-10-21 16:56:37 +0200

    Adding injectable decorator

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,9 +1,10 @@
 import jwt from 'jsonwebtoken'
 import lowdb from 'lowdb'
 import LocalStorage from 'lowdb/adapters/localStorage'
+import { Injectable } from '@angular/core'
 
 const adapter = new LocalStorage('db')
-
+@Injectable({ providedIn: 'root' })
 export class Store {
   static db = new lowdb(adapter)
 
