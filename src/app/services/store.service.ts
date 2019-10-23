d39feb133eb1f23e7c7bbecce6360c0370e54448
wnpraf <rapribu@gmail.com>
2019-10-23 17:30:37 +0200

    Removing injectable decorator for static service

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,11 +1,10 @@
 import lowdb from 'lowdb'
 import LocalStorage from 'lowdb/adapters/LocalStorage'
-import { Injectable } from '@angular/core'
+
 import { decode } from 'jsonwebtoken'
 
 const adapter = new LocalStorage('db')
 
-@Injectable({ providedIn: 'root' })
 export class Store {
   static db: any = lowdb(adapter)
 
