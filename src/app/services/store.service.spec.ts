ecb1bebd5e0cd67d7cf221c1b866cb4b39cb1889
wnpraf <rapribu@gmail.com>
2019-10-24 20:59:46 +0200

    Improving tests with token type matchers

--- src/app/services/store.service.spec.ts
+++ src/app/services/store.service.spec.ts
@@ -1,4 +1,5 @@
 import { Store } from './store.service'
+import { Token } from '../models'
 
 const { db, decode } = Store
 
@@ -13,9 +14,14 @@ describe(' Localstorage', () => {
   })
   it('should decode token', () => {
     const token = decode(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Ik1hcmxlbmUuTXVyYXppa0Bob3RtYWlsLmNvbSIsImlhdCI6MTU3MTMyNjk5MCwiZXhwIjoxNTcxMzM3NzkwfQ.42CynkykWv7lC8oeROu0Uo8Z8Z80DG4z0qPgAgHqjRc'
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Ik1hcmxlbmUuTXVyYXppa0Bob3RtYWlsLmNvbSIsImF2YXRhciI6Imh0dHBzOi8vczMuYW1hem9uYXdzLmNvbS91aWZhY2VzL2ZhY2VzL3R3aXR0ZXIvbmVjb2R5bWljb25lci8xMjguanBnIiwiaWF0IjoxNTcxOTMxNzg4LCJleHAiOjE1NzE5NDI1ODh9.rUY8N9VsCj6MP5_pTui6OJu2IbeJishX2lnmL654T0k'
     )
     db.set('decToken', token).write()
-    expect(db.get('decToken').value()).toHaveProperty('payload')
+    expect(db.get('decToken').value()).toMatchObject<Token>({
+      email: expect.any(String),
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number)
+    })
   })
 })
