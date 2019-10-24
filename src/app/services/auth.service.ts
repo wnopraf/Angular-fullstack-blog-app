11d9f566f797b57d1f772c8eac44cf58013074aa
wnpraf <rapribu@gmail.com>
2019-10-24 20:56:23 +0200

    Making some changes to adapt better to token db manipulation

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core'
 import { Store } from './store.service'
 
+const { db, decode } = Store
+
 @Injectable({ providedIn: 'root' })
 export default class {
-  auth() {
-    if (Store.db.get('token')) return true
-    return false
+  auth(): boolean {
+    if (!db.get('decToken').value()) return false
+    const expTime =
+      db.get('decToken.exp').value() + db.get('decToken.iat').value()
+    if (expTime <= Math.floor(Date.now() / 1000)) {
+      return false
+    } else {
+      return true
+    }
   }
 
-  getUser() {
-    return Store.db.get('token.user')
+  getUser(): { email: string; avatar: string } {
+    const email = db.get('decToken.email').value()
+    const avatar = db.get('decToken.avatar').value()
+    return { email, avatar }
   }
-
-  setToken(token) {
-    Store.db.set('token', token)
+  getRawToken(): string {
+    return db.get('rawToken').value()
+  }
+  setToken(token: string): void {
+    db.set('rawToken', token).write()
+    const decToken = decode(token)
+    db.set('decToken', decToken).write()
   }
 }
