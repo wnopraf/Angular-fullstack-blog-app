ceba4a37f084bda553c8192279b27b5e67496916
wnpraf <rapribu@gmail.com>
2019-10-30 13:21:38 +0100

    Adding reactive from module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { PostExtract } from './components/postExtract/postExtract.component'
 import { PostList } from './components/postList/postList.component'
-import Api from './services/api.service'
-import { HttpClientModule } from '@angular/common/http'
+
 import { PostComponent } from './components/Post/post.component'
 
-import authService from './services/auth.service'
 import { AuthHeader } from './components/authheader/authHeader.component'
 import { LoginComponent } from './components/login/login.component'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -22,8 +22,13 @@ import { LoginComponent } from './components/login/login.component'
     AuthHeader,
     LoginComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [Api],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    ReactiveFormsModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
