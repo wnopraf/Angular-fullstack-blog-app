64009d83677c7f0ef9d964a8a15e12bb905e1b65
wnpraf <rapribu@gmail.com>
2019-10-30 14:05:31 +0100

    Adding login route to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule, Route } from '@angular/router'
 import { PostList } from './components/postList/postList.component'
 import FetchPost from './components/Post/postData.service'
 import { PostComponent } from './components/Post/post.component'
+import { LoginComponent } from './components/login/login.component'
 
 const routes: Routes = [
   { path: '', component: PostList },
-  { path: 'post/:id', component: PostComponent, resolve: { FetchPost } }
+  { path: 'post/:id', component: PostComponent, resolve: { FetchPost } },
+  { path: 'login', component: LoginComponent }
 ]
 
 @NgModule({
