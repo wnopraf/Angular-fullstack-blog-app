f1bf0f8347296ffd3f020ee423f15509a3a8e895
wnpraf <rapribu@gmail.com>
2019-10-30 17:29:22 +0100

    Keeping provider option in component place

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export default class Api {
   constructor(private http: HttpClient) {}
   get<T>(url: string): Observable<T> {
