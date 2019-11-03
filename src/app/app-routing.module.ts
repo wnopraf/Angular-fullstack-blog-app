a58dcd61f9e3e3586403ebebf096be5793895438
wnpraf <rapribu@gmail.com>
2019-11-03 17:26:42 +0100

    Adding dashboard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { PostList } from './components/postList/postList.component'
 import FetchPost from './components/Post/postData.service'
 import { PostComponent } from './components/Post/post.component'
 import { LoginComponent } from './components/login/login.component'
+import { DashBoard } from './components/dashBoard/dashBoard.component'
+import { routeAuth } from './services/routeAuth.service'
 
 const routes: Routes = [
   { path: '', component: PostList },
   { path: 'post/:id', component: PostComponent, resolve: { FetchPost } },
   { path: 'login', component: LoginComponent },
-  { path: 'logout', redirectTo: '' }
+  { path: 'logout', redirectTo: '' },
+  { path: 'dashboard', component: DashBoard, canActivate: [routeAuth] }
 ]
 
 @NgModule({
