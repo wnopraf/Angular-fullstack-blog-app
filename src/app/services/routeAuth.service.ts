38620f804c5592e91f43a944e7627bfb4adf4718
wnpraf <rapribu@gmail.com>
2019-11-05 17:41:09 +0100

    Adding validation route logic

--- src/app/services/routeAuth.service.ts
+++ src/app/services/routeAuth.service.ts
@@ -1,22 +1,28 @@
 import {
   CanActivate,
   RouterStateSnapshot,
-  ActivatedRouteSnapshot
+  ActivatedRouteSnapshot,
+  Router
 } from '@angular/router'
 import authService from './auth.service'
 import auth from 'src/api/auth'
 import { Injectable } from '@angular/core'
+import { router } from 'json-server'
 
 @Injectable({
   providedIn: 'root'
 })
 export class routeAuth implements CanActivate {
-  constructor(private auth: authService) {}
+  constructor(private auth: authService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
-    return this.auth.auth()
+    if (this.auth.auth()) {
+      return true
+    } else {
+      this.router.navigate(['/not-authorized'])
+    }
   }
 }
