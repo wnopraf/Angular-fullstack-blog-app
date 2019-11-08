e04a0b6678622d1cf1d648d59ac103d843f49eb6
wnpraf <rapribu@gmail.com>
2019-11-08 16:11:36 +0100

    Adding depp changes to create component

--- src/app/components/createPost/createPost.component.ts
+++ src/app/components/createPost/createPost.component.ts
@@ -2,26 +2,55 @@ import { Component } from '@angular/core'
 import { FormGroup } from '@angular/forms'
 import Api from 'src/app/services/api.service'
 import Faker from 'faker'
-import { HttpErrorResponse } from '@angular/common/http'
+
+import authService from 'src/app/services/auth.service'
+import { Router } from '@angular/router'
+import { HttpClient } from '@angular/common/http'
+import { Post } from 'src/app/models'
+
 @Component({
   selector: 'create-post',
   templateUrl: './createPost.component.html'
 })
 export class CreatePost {
-  validationServer: HttpErrorResponse = null
-  constructor(private http: Api) {}
-  submit(form: FormGroup) {
-    const { postTile: title, postBody: body } = form.controls
-    const newPost = { image: Faker.image.avatar(), title, body }
+  submit: (form: FormGroup) => void = (form: FormGroup) => {
+    if (!this.auth.auth()) {
+      this.router.navigate(['/not-auth'])
+      return
+    }
     if (form.valid) {
-      this.http.post('http://localhost:3000/posts', newPost).subscribe(
-        next => {
-          console.log('Post posted correctly')
-        },
-        err => {
-          this.validationServer = err
+      const [postTitle, postBody] = [
+        form.get('postTitle'),
+        form.get('postBody')
+      ]
+      const [title, body] = [postTitle.value, postBody.value]
+      const userId = this.auth.getUser().id
+      const rawToken: string = this.auth.getRawToken()
+      const newPost: Post = { userId, image: Faker.image.image(), title, body }
+      const headers = {
+        headers: {
+          authorization: 'bearer ' + rawToken,
+          'Content-Type': 'application/json'
         }
-      )
+      }
+      const jsonPost = JSON.stringify(newPost)
+      this.http
+        .post('http://localhost:3000/posts', jsonPost, headers)
+        .subscribe(
+          next => {
+            console.log('Post posted correctly', next)
+            this.router.navigate(['/user/dashboard'])
+          },
+          err => {
+            console.log(err, 'post error')
+            this.router.navigate(['/server-error'])
+          }
+        )
     }
   }
+  constructor(
+    private http: HttpClient,
+    private auth: authService,
+    private router: Router
+  ) {}
 }
