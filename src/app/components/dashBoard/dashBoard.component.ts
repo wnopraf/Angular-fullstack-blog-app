5e9bf03cc825401c7064313cc22e7efff2a1e686
wnpraf <rapribu@gmail.com>
2019-11-12 13:45:43 +0100

    Fixing fetch data logic issues caused by resolver

--- src/app/components/dashBoard/dashBoard.component.ts
+++ src/app/components/dashBoard/dashBoard.component.ts
@@ -1,18 +1,31 @@
-import { ActivatedRoute } from '@angular/router'
+import { Router, RouterEvent, NavigationEnd } from '@angular/router'
 import { Post } from 'src/app/models'
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+
+import { filter } from 'rxjs/operators'
 import { FetchPosts } from 'src/app/services/fetchPosts.service'
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashBoard.component.html'
 })
-export class DashBoard {
+export class DashBoard implements OnInit {
   userPosts: Post[]
-  constructor(private route: ActivatedRoute) {
-    route.data.subscribe(({ FetchPosts }: { FetchPosts: Post[] }) => {
-      console.log(FetchPosts, 'user posts')
-      this.userPosts = FetchPosts
+  constructor(private router: Router, private fetch: FetchPosts) {}
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event: RouterEvent) => event instanceof NavigationEnd))
+      .subscribe((event: RouterEvent) => {
+        console.log('dashboard navigation', event)
+
+        this.fetch.get().subscribe((data: Post[]) => {
+          console.log('data updated', data)
+          this.userPosts = data
+        })
+      })
+    this.fetch.get().subscribe((data: Post[]) => {
+      console.log('first data load', data)
+      this.userPosts = data
     })
   }
 }
