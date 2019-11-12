d19ed1fcef52dae7c7c15385f9634df3ef597fb6
wnpraf <rapribu@gmail.com>
2019-11-12 13:43:02 +0100

    Turn resolvert to service

--- src/app/services/fetchPosts.service.ts
+++ src/app/services/fetchPosts.service.ts
@@ -1,26 +1,19 @@
-import {
-  Resolve,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot
-} from '@angular/router'
 import { Post } from '../models'
-import Api from './api.service'
+
 import { Observable } from 'rxjs'
 import authService from './auth.service'
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
-export class FetchPosts implements Resolve<Post[]> {
-  constructor(private api: Api, private auth: authService) {}
+export class FetchPosts {
+  constructor(private http: HttpClient, private auth: authService) {}
 
-  resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<Post[]> {
-    const user = this.auth.getUser()
-    const url = `http://localhost:3000/posts?userId=${user.id}&_expand=user`
-    return this.api.get(url)
+  get(): Observable<Post[]> {
+    const userId = this.auth.getUser().id
+    const url = `http://localhost:3000/posts?userId=${userId}&_expand=user`
+    return this.http.get<Post[]>(url)
   }
 }
