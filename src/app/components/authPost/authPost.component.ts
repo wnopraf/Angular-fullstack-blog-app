c860511c21f171efe36b93a7ecb2cde11f4f264a
wnpraf <rapribu@gmail.com>
2019-11-14 14:09:30 +0100

    Extracting id param from route to be able to pass to child component

--- src/app/components/authPost/authPost.component.ts
+++ src/app/components/authPost/authPost.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Params } from '@angular/router'
 
 @Component({
   selector: 'auth-post',
   templateUrl: './authPost.component.html'
 })
-export class AuthPost {}
+export class AuthPost implements OnInit {
+  id: string
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(({ id }: Params) => {
+      this.id = id
+    })
+  }
+}
